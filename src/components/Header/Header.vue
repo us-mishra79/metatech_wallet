fe99fdb855a75aa1d0c5d48ff1693be28aedb17d
muhammad-abdul-rehman <muhammad_abdul_rehman@nextpak.org>
2023-07-02 20:53:19 +0500

    first commit

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <header class="f-header">
+        <div class="narrow-container">
+            <router-link to="/" class="logo" :aria-label="$t('view_home.back_to_home')">
+                <img src="logo.png" alt="" class="not-fluid" />
+            </router-link>
+        </div>
+    </header>
+</template>
+
+<script>
+/**
+ * Renderes header and takes care of navigation.
+ */
+export default {
+    components: {},
+};
+</script>
+
+<style lang="scss">
+@import 'style';
+</style>
